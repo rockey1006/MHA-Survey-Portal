96a33b5e19034b7e75fdad55dd3e612376221097
RainSuds <1319837595@qq.com>
2026-02-20 10:53:25 -0600

    sync survey due date

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -55,6 +55,72 @@ class SurveyTest < ActiveSupport::TestCase
     assert_equal "Fall 2035", survey.semester
   end
 
+  test "changing survey due date updates inherited assignment deadlines but preserves custom ones" do
+    survey = build_survey(title: "Due Sync Survey", semester: "Fall 2036")
+    survey.available_until = Time.zone.local(2036, 10, 1, 12, 0)
+    survey.save!
+
+    inherited_assignment = SurveyAssignment.create!(
+      survey: survey,
+      student: students(:student),
+      advisor: advisors(:advisor),
+      assigned_at: Time.current,
+      available_until: survey.available_until
+    )
+
+    blank_assignment = SurveyAssignment.create!(
+      survey: survey,
+      student: students(:completed_student),
+      advisor: advisors(:other_advisor),
+      assigned_at: Time.current,
+      available_until: nil
+    )
+
+    custom_deadline = Time.zone.local(2036, 10, 20, 17, 0)
+    custom_assignment = SurveyAssignment.create!(
+      survey: survey,
+      student: students(:other_student),
+      advisor: advisors(:other_advisor),
+      assigned_at: Time.current,
+      available_until: custom_deadline
+    )
+
+    new_survey_deadline = Time.zone.local(2036, 11, 15, 9, 30)
+    survey.update!(available_until: new_survey_deadline)
+
+    assert_equal new_survey_deadline.to_i, inherited_assignment.reload.available_until.to_i
+    assert_equal new_survey_deadline.to_i, blank_assignment.reload.available_until.to_i
+    assert_equal custom_deadline.to_i, custom_assignment.reload.available_until.to_i
+  end
+
+  test "removing survey due date clears inherited assignment deadlines" do
+    survey = build_survey(title: "Due Removal Survey", semester: "Spring 2037")
+    survey.available_until = Time.zone.local(2037, 3, 20, 8, 0)
+    survey.save!
+
+    inherited_assignment = SurveyAssignment.create!(
+      survey: survey,
+      student: students(:student),
+      advisor: advisors(:advisor),
+      assigned_at: Time.current,
+      available_until: survey.available_until
+    )
+
+    custom_deadline = Time.zone.local(2037, 4, 10, 8, 0)
+    custom_assignment = SurveyAssignment.create!(
+      survey: survey,
+      student: students(:other_student),
+      advisor: advisors(:other_advisor),
+      assigned_at: Time.current,
+      available_until: custom_deadline
+    )
+
+    survey.update!(available_until: nil)
+
+    assert_nil inherited_assignment.reload.available_until
+    assert_equal custom_deadline.to_i, custom_assignment.reload.available_until.to_i
+  end
+
   private
 
   def build_survey(attrs = {})
